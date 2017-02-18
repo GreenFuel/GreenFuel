b26628e73675ef59da51d6e2d2c21eef2313114d
tangpeng <1084165470@qq.com>
2017-02-18 12:59:47 +0800

    添加“我的”界面

---
+++ app/src/main/java/com/example/tr/greenfuel/mine/MineActivity.java
@@ -0,0 +1,19 @@
+package com.example.tr.greenfuel.mine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.tr.greenfuel.R;
+
+/**
+ * Created by tangpeng on 2017/2/18.
+ */
+
+public class MineActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
