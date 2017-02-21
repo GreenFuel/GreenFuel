72bbc42e2e9b60883bf5d9ad324c74c354628859
TR <244374078@qq.com>
2017-02-21 19:35:32 +0800

    加速度和速度

--- app/src/main/java/com/example/tr/greenfuel/RoutePlan/RouteActivity.java
+++ app/src/main/java/com/example/tr/greenfuel/RoutePlan/RouteActivity.java
@@ -1,6 +1,18 @@
 package com.example.tr.greenfuel.RoutePlan;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.amap.api.navi.AMapNavi;
@@ -12,19 +24,60 @@ import com.example.tr.greenfuel.R;
 import com.example.tr.greenfuel.customView.SpringProgressView;
 import com.example.tr.greenfuel.junge.BaseActivity;
 
-public class RouteActivity extends BaseActivity {
+public class RouteActivity extends BaseActivity implements SensorEventListener{
     private NextTurnTipView mNextTurnTipView;
     private SpringProgressView oilRate;//油耗率
     private SpringProgressView pfRate;//排放率
+    private SpringProgressView speedAnalyze;//排放率
     private TextView nowLeftM;
     private TextView allLeftM;
     private TextView nowRouteName;
-
+    private Location loc;
+    private LocationManager locationManager;
+    private SensorManager sensorManager;
+    private double acc = 0;//加速度
+    private double angle = 0;//角度
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_custom_next_turn_tip_view);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("speed", "onLocationChanged: "+location.getSpeed());
+                loc = location;
+                TextView now = (TextView)findViewById(R.id.now_speed);
+                now.setText("当前速度"+location.getSpeed()*3.6);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
+        initSensor();
         mAMapNaviView = (AMapNaviView) findViewById(R.id.navi_view);
         mAMapNaviView.onCreate(savedInstanceState);
         mAMapNaviView.setAMapNaviViewListener(this);
@@ -32,9 +85,12 @@ public class RouteActivity extends BaseActivity {
         oilRate = (SpringProgressView) findViewById(R.id.oil_pro);
         oilRate.setMaxCount(1000.0f);
         oilRate.setCurrentCount(653);
-        oilRate = (SpringProgressView) findViewById(R.id.pf_pro);
-        oilRate.setMaxCount(1000.0f);
-        oilRate.setCurrentCount(753);
+        pfRate = (SpringProgressView) findViewById(R.id.pf_pro);
+        pfRate.setMaxCount(1000.0f);
+        pfRate.setCurrentCount(753);
+        speedAnalyze = (SpringProgressView) findViewById(R.id.speed_analyze);
+        speedAnalyze.setMaxCount(1000.0f);
+        speedAnalyze.setCurrentCount(582);
         nowLeftM = (TextView) findViewById(R.id.now_left);
         allLeftM = (TextView) findViewById(R.id.all_left);
         nowRouteName = (TextView) findViewById(R.id.now_route_name);
@@ -46,8 +102,8 @@ public class RouteActivity extends BaseActivity {
         mNextTurnTipView = (NextTurnTipView) findViewById(R.id.myDirectionView);
         mAMapNaviView.setLazyNextTurnTipView(mNextTurnTipView);
 
-
-        boolean gps=getIntent().getBooleanExtra("gps", false);
+        boolean gps = getIntent().getBooleanExtra("gps", false);
+        //gps= false;
         mAMapNavi.setEmulatorNaviSpeed(60);
         if(gps){
             mAMapNavi.startNavi(AMapNavi.GPSNaviMode);
@@ -55,6 +111,18 @@ public class RouteActivity extends BaseActivity {
             mAMapNavi.startNavi(AMapNavi.EmulatorNaviMode);
         }
     }
+
+    private void initSensor() {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
+                sensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                sensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
     @Override
     public void onInitNaviSuccess() {
         super.onInitNaviSuccess();
@@ -93,6 +161,23 @@ public class RouteActivity extends BaseActivity {
         nowRouteName.setText(naviinfo.getCurrentRoadName().toString());
         allLeftM.setText(naviinfo.getPathRetainDistance()/1000f+"公里  "+
                 naviinfo.getPathRetainTime()/3600+"小时"+naviinfo.getPathRetainTime()/60%60+"分钟");
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION){
+            angle = sensorEvent.values[1];
+        }
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+            Log.i("acc","angle"+angle+"加速度:"+acc+"\tcos"+Math.cos(2f*3.14159/360f*angle));
+            acc = sensorEvent.values[1]*Math.cos(2f*3.14159/360f*angle);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
