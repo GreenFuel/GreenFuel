0a7cac670d877ad7f3c00d6e331fcb31866b79ff
tangpeng <1084165470@qq.com>
2017-02-24 14:55:27 +0800

    实现了违章查询功能，但是查询次数总共只有100次，所以不要随意使用，查询车型总共10000次，可以随便查询，短信验证每天只能测试20条，请注意次数，还在进行注册不用验证码即可直接注册

--- app/src/main/java/com/example/tr/greenfuel/nearFunction/WeiZhangActivity.java
+++ app/src/main/java/com/example/tr/greenfuel/nearFunction/WeiZhangActivity.java
@@ -1,15 +1,23 @@
 package com.example.tr.greenfuel.nearFunction;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tr.greenfuel.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by tangpeng on 2017/2/23.
@@ -18,54 +26,115 @@ import com.example.tr.greenfuel.R;
 public class WeiZhangActivity extends AppCompatActivity {
 
     private Spinner province;    //省别名
-    private EditText carNumber;  //车牌
-    private EditText carId; //车辆识别号
-    private EditText engineId;  //发动机号
+    private AutoCompleteTextView carNumber;  //车牌
+    private AutoCompleteTextView carId; //车辆识别号
+    private AutoCompleteTextView engineId;  //发动机号
 
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wei_zhang);
+        sharedPreferences = getSharedPreferences("car_number", MODE_PRIVATE);
         initViews();
     }
 
     private void initViews() {
         province = (Spinner) findViewById(R.id.province);
-        carNumber = (EditText) findViewById(R.id.carNumber);
-        carId = (EditText) findViewById(R.id.carId);
-        engineId = (EditText) findViewById(R.id.engineId);
+        carNumber = (AutoCompleteTextView) findViewById(R.id.carNumber);
+        carId = (AutoCompleteTextView) findViewById(R.id.carId);
+        engineId = (AutoCompleteTextView) findViewById(R.id.engineId);
+
+        setTextWatcher(carNumber, "carNumber");
+        setTextWatcher(carId, "carId");
+        setTextWatcher(engineId, "engineId");
+
     }
 
+    private void setTextWatcher(final AutoCompleteTextView completeTextView, final String getStr) {
+
+        completeTextView.addTextChangedListener(new MyTextWater() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                super.onTextChanged(s, start, before, count);
+
+                int number = sharedPreferences.getInt("number", 0);  //保存的记录数目
+                if (number > 0) {//如果有记录
+                    ArrayList<String> arrayList = new ArrayList<String>();
+                    for (int i = 0; i < number; i++) {
+                        arrayList.add(sharedPreferences.getString(getStr + i, null));
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(WeiZhangActivity.this, R.layout.auto_complete_search, arrayList);
+                    completeTextView.setAdapter(arrayAdapter);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+        completeTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                completeTextView.setText(((TextView) view).getText());
+            }
+        });
+    }
+
+
     //查询违章
-    public void searchNow(View v){
+    public void searchNow(View v) {
         String provinceStr = (String) province.getSelectedItem();
         String carNumberStr = carNumber.getText().toString().trim();
         String carIdStr = carId.getText().toString().trim();
         String engineIdStr = engineId.getText().toString().trim();
+        if (provinceStr != null && carNumberStr != null && carNumberStr.length() == 6) {
+            if (carIdStr != null && carIdStr.length() == 17) {
+                if (engineIdStr != null && !engineIdStr.equals("")) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    int number = sharedPreferences.getInt("number", 0);
+                    if (number >= 3) {
+                        number = number % 3;
+                        editor.putInt("number", 3);
+                    } else {
+                        editor.putInt("number", number + 1);
+                    }
+                    editor.putString("carNumber" + number, carNumberStr);
+                    editor.putString("carId" + number, carIdStr);
+                    editor.putString("engineId" + number, engineIdStr);
+                    editor.commit();
 
-        if(provinceStr != null && carNumberStr != null && carNumberStr.length() == 6 )
-        {
-            if( carIdStr != null && carIdStr.length() == 17 ){
-                if(engineIdStr != null && !engineIdStr.equals("")){
-                    Intent intent = new Intent(WeiZhangActivity.this,WeiZhangSearchResultActivity.class);
-                    intent.putExtra("province",provinceStr);
-                    intent.putExtra("carNumber",carNumberStr);
-                    intent.putExtra("carId",carIdStr);
-                    intent.putExtra("engineId",engineIdStr);
+                    Intent intent = new Intent(WeiZhangActivity.this, WeiZhangSearchResultActivity.class);
+                    intent.putExtra("province", provinceStr);
+                    intent.putExtra("carNumber", carNumberStr);
+                    intent.putExtra("carId", carIdStr);
+                    intent.putExtra("engineId", engineIdStr);
                     startActivity(intent);
-                }else{
+                } else {
                     Toast.makeText(this, "请填写发动机号", Toast.LENGTH_SHORT).show();
                 }
-            }else{
+            } else {
                 Toast.makeText(this, "请填全车辆识别号", Toast.LENGTH_SHORT).show();
             }
-        }else{
+        } else {
             Toast.makeText(this, "请填全车牌号", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void back(View v){
+    public void back(View v) {
         finish();
+    }
+
+
+    class MyTextWater implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
     }
 }
--- app/src/main/java/com/example/tr/greenfuel/nearFunction/WeiZhangSearchResultActivity.java
+++ app/src/main/java/com/example/tr/greenfuel/nearFunction/WeiZhangSearchResultActivity.java
@@ -3,21 +3,32 @@ package com.example.tr.greenfuel.nearFunction;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.tr.greenfuel.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tangpeng on 2017/2/23.
@@ -26,7 +37,7 @@ import org.json.JSONObject;
 public class WeiZhangSearchResultActivity extends AppCompatActivity {
 
     private static  final  String BASIC_URL = "http://api.jisuapi.com/illegal/query";
-
+    private static  final String APP_KEY = "64215fcd04e105fd";
     private Intent intent;
     private String province;    //省别名
     private String carNumber;  //车牌
@@ -35,7 +46,33 @@ public class WeiZhangSearchResultActivity extends AppCompatActivity {
 
     private ProgressDialog progressDialog;
     private RequestQueue requestQueue;
+    private String responseResult;  //返回的结果
+
     private ListView listView;
+    private List<Map<String,Object>> mapList;
+    private SimpleAdapter simpleAdapter;
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            dismissProgressDialog();
+            if(msg.what == 1){//获取到有效数据，初始化simpleAdapter
+                initAdapter();
+            }
+            if(msg.what == 2){//没有违章记录
+                TextView tip = (TextView)findViewById(R.id.textview_load_data);
+                tip.setText("没有查到违章记录");
+                tip.setVisibility(View.VISIBLE);
+            }
+            if(msg.what == 3){//获取数据失败
+                findViewById(R.id.layout_load_failed).setVisibility(View.VISIBLE);
+            }
+            if(msg.what == 4){//网络异常
+                findViewById(R.id.layout_load_failed).setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -63,35 +100,96 @@ public class WeiZhangSearchResultActivity extends AppCompatActivity {
     }
 
     private void requestData(){
-        String jsonStr = "{\"lsprefix\":" + "\"" + province + "\"," +
-                "\"lsnum\":" + "\"" + carNumber + "\"," +
-                "\"frameno\":" + "\"" + carId + "\"," +
-                "\"engineno\":" + "\"" + engineId + "\""+
-                "}";
-        JSONObject jsonObject = null;
-        try {
-            jsonObject = new JSONObject(jsonStr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            System.out.println("json转换异常");
-        }
         showProgressDialog();
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, BASIC_URL, jsonObject, new Response.Listener<JSONObject>() {
+//        String jsonStr = "{\"lsprefix\":" + "\"" + province + "\"," +
+//                "\"lsnum\":" + "\"" + carNumber + "\"," +
+//                "\"frameno\":" + "\"" + carId + "\"," +
+//                "\"engineno\":" + "\"" + engineId + "\","+
+//                "\"appkey\":" + "\"" + APP_KEY + "\""+
+//                "}";
+//        JSONObject jsonObject = null;
+//        try {
+//            jsonObject = new JSONObject(jsonStr);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//            System.out.println("json转换异常");
+//        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, BASIC_URL, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONObject jsonObject) {
-                System.out.println(jsonObject.toString());
-                dismissProgressDialog();
+            public void onResponse(String s) {
+                System.out.println(s);
+                responseResult = s;
+                if(s == null || s.length() <= 0)
+                {
+                    handler.sendEmptyMessage(3);
+                }else{
+                    handler.sendEmptyMessage(1);
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("volleyError："+volleyError.getMessage());
-                dismissProgressDialog();
+                handler.sendEmptyMessage(4);
+                System.out.println("返回结果出错");
             }
-        });
-        requestQueue.add(jsonObjectRequest);
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("appkey",APP_KEY);
+                map.put("lsprefix",province);
+                map.put("lsnum",carNumber);
+                map.put("frameno",carId);
+                map.put("engineno",engineId);
+                return map;
+            }
+        };
+
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, BASIC_URL, jsonObject, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject jsonObject) {
+//                System.out.println(jsonObject.toString());
+//                dismissProgressDialog();
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError volleyError) {
+//                System.out.println("volleyError："+volleyError.getMessage());
+//                dismissProgressDialog();
+//            }
+//        });
+//        requestQueue.add(jsonObjectRequest);
+
+        requestQueue.add(stringRequest);
     }
 
+    private void initAdapter(){
+        mapList = new ArrayList<>();
+        if(responseResult != null){
+            try {
+                JSONObject jsonObject = new JSONObject(responseResult);
+                JSONObject result = jsonObject.getJSONObject("result");
+                String listStr = result.getString("list");
+                JSONArray jsonArray = new JSONArray(listStr);
+                for(int i = 0 ; i < jsonArray.length() ;i++){
+                    JSONObject object = jsonArray.getJSONObject(i);
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("time",object.getString("time"));
+                    map.put("address",object.getString("address"));
+                    map.put("content",object.getString("content"));
+                    map.put("price",object.getString("price")+"元");
+                    map.put("score",object.getString("score")+"分");
+                    mapList.add(map);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                System.out.println("json处理异常");
+            }
+        }
+        simpleAdapter = new SimpleAdapter(this,mapList,R.layout.listview_item_wei_zhang,new String[]{"time","address","content","price","score"}
+                ,new int[]{R.id.textview_time,R.id.textview_address,R.id.textview_content,R.id.textview_fine,R.id.textview_diminish_score});
+        listView.setAdapter(simpleAdapter);
+    }
 
     private void showProgressDialog() {
         if (progressDialog == null) {
