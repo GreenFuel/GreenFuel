812f5b13a0c45420ff7e2d4d139395fda999af1a
tangpeng <1084165470@qq.com>
2018-04-20 21:48:09 +0800

    更改了NearActivity命名错误以及更换了 高德api key

--- xCLCharts/build/generated/source/r/androidTest/debug/org/xclcharts/chart/R.java
+++ xCLCharts/build/generated/source/r/androidTest/debug/org/xclcharts/chart/R.java
@@ -8,10 +8,10 @@ package org.xclcharts.chart;
 
 public final class R {
     public static final class drawable {
-        public static final int ic_launcher = 0x7f020000;
+        public static final int ic_launcher = 0x7f010000;
     }
     public static final class string {
-        public static final int app_name = 0x7f040000;
+        public static final int app_name = 0x7f020000;
     }
     public static final class style {
         public static final int AppBaseTheme = 0x7f030000;
